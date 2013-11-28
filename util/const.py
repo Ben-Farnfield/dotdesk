f0fc51dcf3bfa5f9222352f04e1699d9ef452561
Ben-Farnfield <ben.farnfield@gmail.com>
2013-11-28 09:36:36 +0000

    Added new module to hold all the programs constants. This was added to the util package.

---
+++ util/const.py
@@ -0,0 +1,39 @@
+
+'''
+
+'''
+
+import getpass
+
+# Current users home directory.
+HOME = "/home/" + getpass.getuser() + "/"
+
+
+# Default icon theme for global install.
+ICON_THEME = "/usr/share/icons/hicolor/"
+
+
+# Icon install directories.
+GLOBAL_ICON_DIR = ICON_THEME + "{size}/apps/"
+LOCAL_ICON_DIR = HOME + ".icons/"
+
+
+# .desktop install directories.
+GLOBAL_DESK_DIR = "/usr/share/applications/"
+LOCAL_DESK_DIR = HOME + ".local/share/applications/"
+
+
+#
+CATEGORIES_LIST = ["AudioVideo",
+                   "Audio",
+                   "Video",
+                   "Development",
+                   "Education",
+                   "Game",
+                   "Graphics",
+                   "Network",
+                   "Office",
+                   "Science",
+                   "Settings",
+                   "System",
+                   "Utility"]
