643108288db4400945689a6331115fa6560cb271
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-01 18:43:34 +0000

    Removed error msg arguments.

--- interface/prompt.py
+++ interface/prompt.py
@@ -9,8 +9,7 @@ def for_string(prompt):
     string = raw_input("\n> " + prompt + ": ")
     if string is None:
         return ""
-    else:
-        return string
+    return string
 
 def for_yes_no(prompt):
     while True:
@@ -19,20 +18,20 @@ def for_yes_no(prompt):
             return True
         if yes_no in ("n", "N", "no", "No"):
             return False
-        print "\nMust enter y or n"
+        print "\nMust enter y or n."
 
-def for_path(prompt, error):
+def for_path(prompt):
     while True:
         path = for_string(prompt)
         if utils.file_exists(path):
             return path
         else:
-            print "\n" + error
+            print "\nFile not found."
 
-def to_select(prompt, error, num_options):
+def to_select(prompt, num_options):
     while True:
         select = int(for_string(prompt))
         if select >= 0 and select < num_options:
             return select
         else:
-            print "/n" + error
+            print "/nMust select an option between 0 and " + num_options-1
