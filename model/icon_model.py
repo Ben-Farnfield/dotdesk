782fbcf98b4e7296668d79723f998b8ddceca060
Ben-Farnfield <ben.farnfield@gmail.com>
2013-12-02 11:39:49 +0000

    Added install_icon flag to the icon model.

--- model/icon_model.py
+++ model/icon_model.py
@@ -1,16 +1,19 @@
-import const
+from const import ICON_INSTALL_DIR
 
 class IconModel(object):
     ''' This class models the icon to be installed. '''
-    def __init__(self, name=""):
-        self.program_name = name
+    def __init__(self, icon_name=""):
+        self.install_icon = False
+
+        self.icon_to_install = ""
+
+        self.icon_name = icon_name
         self.icon_type = ""
         self.icon_size = ""
-        self.icon_to_install = ""
 
     def __str__(self):
         # put assert in here !
-        install_dir = const.ICON_INSTALL_DIR.format(icon_size=self.icon_size)
-        icon_name = self.program_name + self.icon_type
+        install_dir = ICON_INSTALL_DIR.format(icon_size=self.icon_size)
+        icon = self.icon_name + self.icon_type
         
-        return install_dir + icon_name
+        return install_dir + icon
