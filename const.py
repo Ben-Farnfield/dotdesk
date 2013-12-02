012c6fa8e2b9f756220a8567d482ec2efcdc28a2
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-02 05:24:57 +0000

    Changed comment from lists to tuples

--- const.py
+++ const.py
@@ -10,7 +10,7 @@ DESK_INSTALL_DIR = "/usr/share/applications/"
 ICON_THEME = "/usr/share/icons/hicolor/"
 ICON_INSTALL_DIR = ICON_THEME + "{icon_size}/apps/"
 
-    # ------------------------- selection lists ------------------------- #
+    # ------------------------- selection tuples ------------------------ #
 
 CATEGORIES = ("AudioVideo",
               "Audio",
