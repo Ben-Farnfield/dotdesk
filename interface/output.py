e55eede6592b2fa2d3cf2b6914b327cbaeba1498
Ben-Farnfield <ben.farnfield@gmail.com>
2013-12-02 13:24:16 +0000

    Added \n to end of prompt.

--- interface/output.py
+++ interface/output.py
@@ -4,6 +4,6 @@
 '''
 
 def option_list(prompt, list_to_print):
-    print "\n" + prompt + ":"
+    print "\n" + prompt + ":\n"
     for i in range(len(list_to_print)):
         print "{0:>4}.{1:.>12}".format("[" + str(i) + "]", list_to_print[i])
