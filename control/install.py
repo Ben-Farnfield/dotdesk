5f6f5f8dd2fb59d54546c04609599f440835a9ca
Ben-Farnfield <ben.farnfield@gmail.com>
2013-12-02 11:29:03 +0000

    Implemented new icon model within install cli function.

--- control/install.py
+++ control/install.py
@@ -8,26 +8,26 @@ import const
 import interface.prompt
 import interface.output
 from model.dot_desktop_model import DotDesktopModel
+from model.icon_model import IconModel
 
 import sys
 
 def install(args):
-    icon_to_install = None
-    icon_type = None
-    icon_size = None
-
     program = args.i
-    
+
     if utils.file_exists(const.DESK_INSTALL_DIR + program + ".desktop"):
         print program + ".desktop is already installed."
         sys.exit()
 
     desktop = DotDesktopModel(program)
-    desktop = run_install_cli(desktop)
-    run_icon_install(desktop)
+    icon = IconModel(program)
+    
+    desktop, icon = run_install_cli(desktop, icon)
+    run_icon_install(icon)
     run_desktop_install(desktop)
 
-def run_install_cli(desktop):
+
+def run_install_cli(desktop, icon):
     desktop.terminal = prompt.for_yes_no("Terminal app?")
     desktop.tooltip = prompt.for_string("Enter tooltip")
     desktop.exe = prompt.for_string("Enter execution command")
@@ -38,21 +38,20 @@ def run_install_cli(desktop):
 
     install_icon = prompt.for_yes_no("Install an icon?")
     if install_icon:
-        icon_to_install = prompt.for_path("Enter full path to icon")
-        icon_type = utils.file_type(icon_to_install)
+        icon.icon_to_install = prompt.for_path("Enter full path to icon")
+        icon.icon_type = utils.file_type(icon.icon_to_install)
 
         output.option_list("Select an icon size", const.ICON_SIZES)
         selection = prompt.for_selection("Make selection",
                                          const.ICON_SIZES_LEN)
-        icon_size = const.ICON_SIZES[selection]
+        icon.icon_size = const.ICON_SIZES[selection]
         
-    return desktop
+    return (desktop, icon)
 
-def run_icon_install(desktop):
-    icon_name = program + icon_type
-    install_dir = const.ICON_INSTALL_DIR.format(icon_size=icon_size)
-    
+
+def run_icon_install(icon):
     installed = utils.copy_file(icon_to_install, install_dir + icon_name)
+
 
 def run_desktop_install():
     pass
