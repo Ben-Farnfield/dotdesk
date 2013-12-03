37368c6a7f74c0a59e6efff63a8ac414330dd839
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-03 08:01:11 +0000

    Added function which outputs a line of 30 dashes.

--- interface/output.py
+++ interface/output.py
@@ -7,3 +7,6 @@ def option_list(prompt, list_to_print):
     print "\n" + prompt + ":\n"
     for i in range(len(list_to_print)):
         print "{0:>4}.{1:.>12}".format("[" + str(i) + "]", list_to_print[i])
+
+def line():
+    print "\n{:-<30}\n".format("")
