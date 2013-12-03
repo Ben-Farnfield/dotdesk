5fa446bb626a1a2d860c8f8e54a31bd08e8dbfd1
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-03 17:25:54 +0000

    Added try-except for KeyboardInterrupt so can cleanly exit with ctrl+c.

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ from control.remove import remove
 
 import sys
 
-# Get command line args before checking user privileges so the -h command can
-# be run by any user.
-args = utils.get_args()
+def main():
+    # Get command line args before checking user privileges so the -h command can
+    # be run by any user.
+    args = utils.get_args()
 
-if utils.is_NOT_root_user():
-    print "You do not have the correct privileges. Please run as 'root' user."
-    sys.exit()
+    if utils.is_NOT_root_user():
+        print "You do not have the correct privileges. Please run as 'root' user."
+        sys.exit()
 
-if args.i:
-    install(args)
-elif args.r:
-    remove(args)
-else:
-    print "Try 'dotdesk -h'"
+    if args.i:
+        install(args)
+    elif args.r:
+        remove(args)
+    else:
+        print "Try 'dotdesk -h'"
+
+try:
+    main()
+except KeyboardInterrupt:
+    print "\n"
