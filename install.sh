4618001efdb2bb1956477122c62bd7ab9b87fdbb
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-07 12:51:33 +0000

    New install script has been written. The new script creates a sym link rather than a bash script within /usr/bin

---
+++ install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+################################
+# This script installs dotdesk #
+################################
+
+DIR=$(pwd)
+
+if [ "$1" = "-i" ]; then
+    if [ -f /usr/bin/dotdesk ]; then
+        printf "dotdesk is already installed\n"
+    else
+        sudo ln -s $DIR/main.py /usr/bin/dotdesk
+        printf "dotdesk has been installed\n"
+    fi
+elif [ "$1" = "-r" ]; then
+    if [ -f /usr/bin/dotdesk ]; then
+        sudo rm /usr/bin/dotdesk
+        printf "dotdesk has been removed\n"
+    else
+        printf "dotdesk is not installed\n"
+    fi
+else
+    printf "Try: 'sudo sh install.sh [-i OR -r]'\n"
+fi
